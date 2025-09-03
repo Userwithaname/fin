a5df2875f1a45dd50219b8ba2e9229504e022591
Userwithaname <42674728+Userwithaname@users.noreply.github.com>
2025-09-03 17:08:42 +0200

    Cleanup `test_readme.rs`

--- tests/test_readme.rs
+++ tests/test_readme.rs
@@ -1,30 +1,24 @@
-macro_rules! readme_path {
-    () => {
-        env!("CARGO_MANIFEST_DIR").to_owned() + "/README.md"
-    };
-}
-
 mod test_readme {
     #![cfg(test)]
 
     use std::fs;
 
+    #[inline]
+    fn readme() -> String {
+        let readme_path = env!("CARGO_MANIFEST_DIR").to_owned() + "/README.md";
+        fs::read_to_string(&readme_path).expect(&format!("`{readme_path}` is not a valid path"))
+    }
+
     #[test]
     fn consistent_help_message() {
-        let readme = fs::read_to_string(readme_path!())
-            .expect(&format!("`{}` is not a valid path", readme_path!()));
-
-        if !readme.contains(&fin::args::show_help()) {
+        if !readme().contains(&fin::args::show_help()) {
             panic!("Help message in the README needs to be updated");
         }
     }
 
     #[test]
     fn consistent_config_file() {
-        let readme = fs::read_to_string(readme_path!())
-            .expect(&format!("`{}` is not a valid path", readme_path!()));
-
-        if !readme.contains(&fin::default_config!()) {
+        if !readme().contains(&fin::default_config!()) {
             panic!("Example configuration in the README needs to be updated");
         }
     }
