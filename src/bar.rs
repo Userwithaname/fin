12110bfe96f9f4e5230174f4e13d2c9fbba84596
Userwithaname <42674728+Userwithaname@users.noreply.github.com>
2025-09-07 05:22:40 +0200

    Revised progress bars

--- src/bar.rs
+++ src/bar.rs
@@ -1,16 +1,18 @@
-const BAR_SIZE: u8 = 20;
+const BAR_SIZE: usize = 20;
 
 pub fn show_progress(prefix: &str, progress: f64, suffix: &str) {
     use std::io::{stdout, Write};
 
-    let mut bar = String::new();
-    for i in 0..BAR_SIZE {
-        bar += match (i as f64 - (progress * BAR_SIZE as f64)).round() {
-            x if x < 0.0 => "—",
-            0.0 => "→",
-            _ => " ",
-        }
-    }
+    let cur_pos = (progress * BAR_SIZE as f64).round() as usize;
+    let remainder = match BAR_SIZE > cur_pos {
+        true => BAR_SIZE - cur_pos,
+        false => 0,
+    };
+
+    let bar = match progress < 0.995 && cur_pos > 0 {
+        true => "—".repeat(cur_pos - 1) + &"→" + &" ".repeat(remainder),
+        false => "—".repeat(cur_pos) + &" ".repeat(remainder),
+    };
 
     print!("\r{prefix} [{bar}] {suffix}");
     let _ = stdout().flush();
